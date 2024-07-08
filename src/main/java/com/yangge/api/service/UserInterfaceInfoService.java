510e1714ef7b1a12f279cedcfd3045f2ae526e9d
youthOfMe <2040690180@qq.com>
2024-07-08 23:26:58 +0800

    feat: 完成生成用户-接口多对多表的代码生成

---
+++ src/main/java/com/yangge/api/service/UserInterfaceInfoService.java
@@ -0,0 +1,13 @@
+package com.yangge.api.service;
+
+import com.yangge.api.model.entity.UserInterfaceInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 20406
+* @description 针对表【user_interface_info(用户调用接口关系)】的数据库操作Service
+* @createDate 2024-07-08 23:21:39
+*/
+public interface UserInterfaceInfoService extends IService<UserInterfaceInfo> {
+
+}
