c5abfa379c7648b5e415b618c1d5374cd09d4bca
youthOfMe <2040690180@qq.com>
2024-07-08 23:33:42 +0800

    feat: 初步完成用户-接口关系的增删改查

---
+++ src/main/java/com/yangge/api/model/dto/userInterfaceInfo/UserInterfaceInfoAddRequest.java
@@ -0,0 +1,39 @@
+package com.yangge.api.model.dto.userInterfaceInfo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 创建请求
+ *
+
+ */
+@Data
+public class UserInterfaceInfoAddRequest implements Serializable {
+
+    /**
+     * 调用用户 id
+     */
+    private Long userId;
+
+    /**
+     * 接口 id
+     */
+    private Long interfaceInfoId;
+
+    /**
+     * 总调用次数
+     */
+    private Integer totalNum;
+
+    /**
+     * 剩余调用次数
+     */
+    private Integer leftNum;
+
+    /**
+     * 0-正常，1-禁用
+     */
+    private Integer status;
+}
---
+++ src/main/java/com/yangge/api/model/dto/userInterfaceInfo/UserInterfaceInfoUpdateRequest.java
@@ -0,0 +1,43 @@
+package com.yangge.api.model.dto.userInterfaceInfo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 更新请求
+ *
+ */
+@Data
+public class UserInterfaceInfoUpdateRequest implements Serializable {
+
+    /**
+     * ID
+     */
+    private Long id;
+
+    /**
+     * 调用用户 id
+     */
+    private Long userId;
+
+    /**
+     * 接口 id
+     */
+    private Long interfaceInfoId;
+
+    /**
+     * 总调用次数
+     */
+    private Integer totalNum;
+
+    /**
+     * 剩余调用次数
+     */
+    private Integer leftNum;
+
+    /**
+     * 0-正常，1-禁用
+     */
+    private Integer status;
+}
